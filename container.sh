961261f8f04afe3a41dccb27d512f19cd07eea0e
Walterb0b <oswald.carstensen@gmail.com>
2026-09-24 09:29:33 +0200

    fixes

--- container.sh
+++ container.sh
@@ -3,7 +3,7 @@
 set -e
 
 CONTAINER_NAME="nginx-container"
-
+#Check for running container
 if[ "$(docker ps -aq -f name=^/${CONTAINER_NAME})" ]; then
   echo "Removing existing container"
   docker rm -f $CONTAINER_NAME
