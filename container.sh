8ac91ef8465bc94dd73261aca9241d9dbf644ebb
Walterb0b <oswald.carstensen@gmail.com>
2026-09-24 09:53:44 +0200

    check logs

--- container.sh
+++ container.sh
@@ -10,3 +10,5 @@ docker rm -f $CONTAINER_NAME 2>/dev/null || true
 docker run -d --name $CONTAINER_NAME -p 80:80 nginx:latest
 
 docker exec $CONTAINER_NAME ls -la /usr/share/nginx/html
+
+docker logs $CONTAINER_NAME
