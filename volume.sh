04eb05d71851a6cdf8c4ddebc05256632d6b1dd6
Walterb0b <oswald.carstensen@gmail.com>
2026-09-24 11:29:06 +0200

    extended the volume script

--- volume.sh
+++ volume.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo "Building docker image"
+docker build -t my-custom-python .
+
 VOLUME_NAME="my-shared-volume"
 MESSAGE="Hello from container"
 
@@ -9,15 +12,15 @@ docker volume rm -f $VOLUME_NAME 2>/dev/null || true
 docker volume create "my-shared-volume"
 
 echo "Container writing message"
-docker run --rm -v my-shared-volume:/data python:alpine sh -c "echo '$MESSAGE' > /data/message.txt"
+docker run --rm -v my-shared-volume:/app/data my-custom-python sh -c "echo '$MESSAGE' > /app/data/message.txt"
 
 echo "Reading message from volume"
-READ_MESSAGE=$(docker run --rm -v my-shared-volume:/data python:alpine cat /data/message.txt)
+READ_MESSAGE=$(docker run --rm -v my-shared-volume:/app/data my-custom-python cat /app/data/message.txt)
 
 echo "Recieved message: $READ_MESSAGE"
 
 echo "Trying to read from container not on the volume"
-if docker run --rm python:alpine test -f /data/message.txt; then
+if docker run --rm my-custom-python test -f /app/data/message.txt; then
   echo "File was found even though the volume was not mounted"
   exit 1
 else
