0ee373d438a932054d858208b0e9e1e8dc87eed5
Walterb0b <oswald.carstensen@gmail.com>
2026-09-24 11:22:27 +0200

    extended the volume script

--- volume.sh
+++ volume.sh
@@ -16,4 +16,12 @@ READ_MESSAGE=$(docker run --rm -v my-shared-volume:/data python:alpine cat /data
 
 echo "Recieved message: $READ_MESSAGE"
 
+echo "Trying to read from container not on the volume"
+if docker run --rm python:alpine test -f /data/message.txt; then
+  echo "File was found even though the volume was not mounted"
+  exit 1
+else
+  echo "Succes: The container could not find the file (expected)"
+fi
+
 docker volume rm -f $VOLUME_NAME
