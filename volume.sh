bcee5f13f46bbb47399128cd2b4e1bc684c16e2d
Walterb0b <oswald.carstensen@gmail.com>
2026-09-24 11:42:14 +0200

    extended the volume script

--- volume.sh
+++ volume.sh
@@ -12,7 +12,7 @@ docker volume rm -f $VOLUME_NAME 2>/dev/null || true
 docker volume create "my-shared-volume"
 
 echo "Container writing message"
-docker run --rm -v my-shared-volume:/app/data my-custom-python sh -c "echo '$MESSAGE' > /app/data/message.txt"
+docker run --name container_1 -v my-shared-volume:/app/data my-custom-python sh -c "echo '$MESSAGE' > /app/data/message.txt"
 
 echo "Reading message from volume"
 READ_MESSAGE=$(docker run --rm -v my-shared-volume:/app/data my-custom-python cat /app/data/message.txt)
@@ -22,5 +22,7 @@ echo "Recieved message: $READ_MESSAGE"
 READ_MESSAGE_2=$(docker run --rm -v my-shared-volume:/app/data my-custom-python cat /app/data/message.txt)
 
 echo "Recieved message: $READ_MESSAGE_2"
+
+docker rm -f container_1
 
 docker volume rm -f $VOLUME_NAME
