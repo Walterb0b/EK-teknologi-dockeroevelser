80aa3a531ce3bd128da07f9a6816e6770c0e9a80
Walterb0b <oswald.carstensen@gmail.com>
2026-09-24 10:00:31 +0200

    second container

--- container.sh
+++ container.sh
@@ -2,13 +2,20 @@
 
 set -e
 
-CONTAINER_NAME="nginx-container"
+CONTAINER_1="nginx-container-1"
+CONTAINER_2="nginx-container-2"
 #Check for running container
+echo "Removing containers"
+docker rm -f $CONTAINER_1 $CONTAINER_2 2>/dev/null || true
 
-docker rm -f $CONTAINER_NAME 2>/dev/null || true
+echo "Starting container ($CONTAINER_1)"
+docker run -d --name $CONTAINER_1 -p 80:80 nginx:latest
 
-docker run -d --name $CONTAINER_NAME -p 80:80 nginx:latest
+echo "Contents of /usr/share/nginx/html in $CONTAINER_1"
+docker exec $CONTAINER_1 ls -la /usr/share/nginx/html
 
-docker exec $CONTAINER_NAME ls -la /usr/share/nginx/html
+echo "Checking logs of $CONTAINER_1"
+docker logs $CONTAINER_1
 
-docker logs $CONTAINER_NAME
+echo "Starting container ($CONTAINER_2)"
+docker run -d --name $CONTAINER_2 -p 8080:80 nginx:latest
